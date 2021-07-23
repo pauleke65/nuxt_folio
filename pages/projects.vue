26c26c9b70ba9d82de5cb5a9fb3716ae8c6e201e
Paul Imoke <pauleke65@gmail.com>
2021-07-23 17:06:11 +0000

    multi projects on page

--- pages/projects.vue
+++ pages/projects.vue
@@ -5,14 +5,16 @@
 
 <div class="h-screen pt-6 px-6 md:px-64">
     
-    <div class="flex my-4">
+    <div class="flex my-4 "  v-for="project in Projects"
+        :key="project.id">
         <div class="flex-initial">
             <img class="w-36 h-36 mr-6" src="../assets/images/nativescript-vue.svg" alt="">
         </div>
-        <div class="flex-1">
-            <h2 class="font-bold text-lg md:text-3xl">{{ Projects[0].project_name}}</h2>
-            <div v-if="error">{{ error }}</div>
-            <p class="mt-2 font-semibold">{{ Projects[0].project_subtitle}}</p>
+        <div class="flex-1"
+       >
+            <h2 class="font-bold text-lg md:text-3xl">{{ project.project_name}}</h2>
+            
+           <p class="mt-2 font-semibold">{{ project.project_subtitle}}</p>
             <div class="flex">
                 <p class="my-1 mr-2">Built with ❤ using:</p>
                 <img
@@ -24,7 +26,7 @@
 
             </div>
 
-            <NuxtLink class=" text-blue-500" :to="{ path: '/project/' + Projects[0].project_id }">Find Out More Here ></NuxtLink>
+            <NuxtLink class=" text-blue-500" :to="{ path: '/project/' + project.project_id }">Find Out More Here ></NuxtLink>
         </div>
     </div>
 
